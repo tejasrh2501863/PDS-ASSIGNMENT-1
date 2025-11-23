ab052bf85d2199ea30ff28aabd2474a3cf1a9f82
TEJAS RH <tejasrh007@gmail.com>
2025-11-23 17:39:20 +0530

    1Q.py

--- 1Q.py
+++ 1Q.py
@@ -1,7 +1,6 @@
-"""1.Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle.
-if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle.
-if they want to travel three hundred miles or more tell them to drive Super-Car"""
-
+#1.Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle.
+#if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle.
+#if they want to travel three hundred miles or more tell them to drive Super-Car
 
 distance = float(input("How far do you want to travel (in miles)? "))
 
@@ -12,4 +11,5 @@ elif distance < 300:
     print("You should ride a Motor-Cycle.")
 else:
     print("You should drive a Super-Car.")
+
 
